75ba0e164dd13effa55d20167ac58deaccc34da9
Jackson Toenjes <jtoenjes34@gmail.com>
2022-09-12 13:27:28 -0500

    Create loanCalculator.java

---
+++ Java/loanCalculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int amount = scanner.nextInt();
+		//your code goes here
+		
+	}
+}
